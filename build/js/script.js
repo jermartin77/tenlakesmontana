86f23d3c9b78364e6a97866a40152533579c1632
Jeremiah Martin <jeremiah@zaneray.com>
2015-11-14 09:59:31 -0700

    mega changes .

--- build/js/script.js
+++ build/js/script.js
@@ -1,38 +1,79 @@
-$(function(){
-  var introMap = function(){
-    var waypoint = new Waypoint({
-      element: document.getElementById('intro-map'),
-      handler: function(direction) {
-        $('#svg-map').addClass('map-active');
+var introMap = function(){
+  var waypoint = new Waypoint({
+    element: document.getElementById('intro'),
+    handler: function(direction) {
+      $('#svg-map').addClass('map-active');
 
-        setTimeout(function(){
-          mapIncrement(trailsMax);
-        },50);
+      setTimeout(function(){
+        mapIncrement(trailsMax);
+      },50);
 
 
-      }
-    })
-  };
+    },
+    offset: '10%'
+  });
+};
 
-  var $mapCounter = $('#map-counter'),
-  currentCount = $mapCounter.text(),
-  trailsMax = 86,
-  trailsMin = 16;
 
-  var mapIncrement = function(count) {
-    if(currentCount <= trailsMax && count === trailsMax) {
-      currentCount++;
-     
-    }
-    else if (currentCount => trailsMin){
-      currentCount--;
+var winWidth;
+
+
+var stickyNav = function(){
+  var $navigationLink = $('#navigation').find('.navigation-link');
+
+  //waypoints to trigger the  and the navigation higlighting. 
+  $('.navigation-section').waypoint({
+    handler: function() {
+      var theElement = this.element,
+      id = theElement.id;
+
+      $navigationLink.removeClass('active');
+      $('#navigation-link-' + id).addClass('active');
+
+
+
       
-   }
-   $mapCounter.text(currentCount);
-   setTimeout(function(){mapIncrement(count);},50);
- }
+    },
+    offset: 0
+  });
 
- var toggleMaps = function(){
+
+
+
+  $("#navigation").stick_in_parent();
+};
+
+var $mapCounter = $('#map-counter'),
+currentCount = $mapCounter.text(),
+trailsMax = 86,
+trailsMin = 16,
+t; //store the time out. 
+
+var mapIncrement = function(count) {
+  clearTimeout(t);
+  if(count === trailsMax) {
+    //count up
+    if(currentCount < trailsMax) {
+      currentCount++;
+      t = setTimeout(function(){mapIncrement(count);},25);
+
+    }
+    
+  }
+  else {
+    //count down  
+
+    if(currentCount > trailsMin) {
+      currentCount--;
+      t = setTimeout(function(){mapIncrement(count);},25);
+
+    }
+  }
+  $mapCounter.text(currentCount);
+  
+}
+
+var toggleMaps = function(){
   var $mapToggleLink = $('#map-toggle .map-toggle-link'),
   $mapToggleBg = $('#map-toggle-bg'),
   $svgMap = $('#svg-map');
@@ -49,23 +90,33 @@ $(function(){
     else {
       $mapToggleBg.removeAttr('style');
       $svgMap.addClass('trails-open').removeClass('trails-closed');
-       mapIncrement(trailsMax);
+      mapIncrement(trailsMax);
     }
     $(this).addClass('active').siblings('.map-toggle-link').removeClass('active');
   });
 }
 
 
+var routesAccordion = function(){
+  $('.route-header').on('click',function(){
+    if($(window).width() < 480) {
+      $(this).toggleClass('active').next('.route-contents').slideToggle(300);
+    }
+  });
+};
 
 
-$('#griderly').griderly();
+$(function(){
 
-$('.navigation-link').scrollToID({
-  offset: 0
-});
+  $('#griderly').griderly();
 
-introMap();
-toggleMaps();
+  $('.navigation-link').scrollToID({
+    offset: 0
+  });
 
-$("#navigation").stick_in_parent();
+  introMap();
+  toggleMaps();
+  stickyNav();
+  routesAccordion();
+  
 });
